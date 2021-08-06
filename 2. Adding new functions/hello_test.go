63f57253c4866538bd97c421e533998484e7a8c5
bujosa <davidbujosa@gmail.com>
2021-08-06 13:19:01 -0400

    Fixed test

--- 2. Adding new functions/hello_test.go
+++ 2. Adding new functions/hello_test.go
@@ -2,16 +2,16 @@ package hello
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	want := "Hello, world."
-	if got := Hello(); got != want {
-		t.Errorf("Hello() = %q, want %q", got, want)
-	}
-}
-
 func TestProverb(t *testing.T) {
 	want := "Concurrency is not parallelism."
 	if got := Proverb(); got != want {
 		t.Errorf("Proverb() = %q, want %q", got, want)
+	}
+}
+
+func TestGlass(t *testing.T) {
+	want := "I can eat glass and it doesn't hurt me."
+	if got := Glass(); got != want {
+		t.Errorf("Glass() = %q, want %q", got, want)
 	}
 }
